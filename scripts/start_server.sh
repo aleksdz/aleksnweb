742ab02553c0cff2b8976492a23e05c488844749
Aleks <zaersx@gmail.com>
2019-07-24 23:15:29 +0100

    declared bash in start scripts

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 dotnet run --project /var/aleksnweb/WebServer/WebServer.fsproj > >(logger -p user.info) 2> >(logger -p user.warn)
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 kill $(ps aux | grep 'WebServer.dll' | awk '{print $2}')
