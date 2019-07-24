b3181e90f9de083ecba3e280e90eedd013fbd88c
Aleks <zaersx@gmail.com>
2019-07-25 00:39:08 +0100

    nohup dotnet command instead of script

--- scripts/appstart.sh
+++ scripts/appstart.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup ./scripts/start_server.sh
+/scripts/start_server.sh > /dev/null 2> /dev/null < /dev/null &
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dotnet run --project /var/aleksnweb/WebServer/WebServer.fsproj &
+nohup dotnet run --project /var/aleksnweb/WebServer/WebServer.fsproj &
